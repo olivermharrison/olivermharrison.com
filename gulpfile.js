888835648a2d7e83c847a66eef408e5425f0685d
Oliver Harrison <olivermharrison@gmail.com>
2017-07-04 20:50:38 +0100

    [+] Basic gulp file and setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
 var gulp = require('gulp');
+var del = require('del');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-clean-css');
+var useref = require('gulp-useref');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+gulp.task('build', ['clean'], function() {
+
+  gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
+  .pipe(gulp.dest('build/img/'));
+
+  gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCSS()))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function(){
+     return del('build/**', {force:true});
 });
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+smoothScroll.init({
+    // Speed & Easing
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    offset: function(){ return window.innerHeight * 0.2 + 20;}, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+    easing: 'easeInOutCubic', // Easing pattern to use
+
+    // Callback API
+    before: function (anchor, toggle) {}, // Function to run before scrolling starts
+    after: function (anchor, toggle) {} // Function to run after scrolling completes
+    });
